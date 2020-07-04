fd45d1829c820887d115848ecc8d350b136bd6ce
Kylinxs <120146398+Kylinxs@users.noreply.github.com>
2020-07-04 13:52:55 +0000

    hrtimer: Allow concurrent hrtimer_start() for self restarting timers

---
+++ lib/setup/default_homepage.php
@@ -0,0 +1,23 @@
+<?php
+
+// (c) Copyright by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+if (basename($_SERVER['SCRIPT_NAME']) === basename(__FILE__)) {
+    die('This script may only be included.');
+}
+
+$groupHome = $userlib->get_user_default_homepage($user);
+if ($groupHome != '') {
+    if (! preg_match('/^(\/|https?:)/', $groupHome)) {
+        $prefs['wikiHomePage'] = $groupHome;
+        $wikilib = TikiLib::lib('wiki');
+        $prefs['tikiIndex'] = $wikilib->sefurl($prefs['wikiHomePage']);
+        $smarty->assign('wikiHomePage', $prefs['wikiHomePage']);
+    } else {
+        $prefs['tikiIndex'] = $groupHome;
+    }
+}
